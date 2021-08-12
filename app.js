4968bd37f09f34f229f0eed1aea9c0d642fd9d06
redpandaathome <leeyumi415@gmail.com>
2021-08-12 19:24:07 +0900

    Organize variable name & done - capture-organizer

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 fs = require('fs');
 const method = process.argv.slice(2)[0];
-const folder = process.argv.slice(3)[0];
+const sourcePath = process.argv.slice(3)[0];
+const targetPath = process.argv.slice(4)[0];
+
 const ellieOrganizer = require("./lib/ellie-organizer.js");
 const caputreOrganizer = require("./lib/capture-organizer.js")
+const dailyOrganizer = require("./lib/daily-organizer.js");
 main()
 
-// run ex - node app.js ellie test
-// run ex - node app.js screenshot Screenshot
+// run ex - node app.js ellie /Users/yumi/Downloads/life/photo/test/ _
+// run ex - node app.js screenshot /Users/yumi/Desktop/ /Users/yumi/Desktop/Screenshot/
+// run ex - node app.js daily Aug
 
 // FULL_PATH ex - export FULL_PATH="/Users/yumi/Downloads/life/photo/"
 function main(){
    console.log("🥁🥁🥁🥁🥁")
-   const fullPath = process.env.FULL_PATH + folder + "/";
-   const deskTopPath = '/Users/yumi/Desktop/';
-   console.log(`Processing in ${fullPath}`)
+   console.log(`Processing in ${sourcePath}`)
    if (method == 'ellie'){
-      ellieOrganizer.organizer(fullPath)
+      ellieOrganizer.organizer(sourcePath)
    } else if ( method == 'screenshot'){
-      caputreOrganizer.organizer(deskTopPath, folder)
+      caputreOrganizer.organizer(sourcePath, targetPath)
+   } else if ( method == 'daily'){
+      dailyOrganizer.organizer(fullPath)
    }
 }
