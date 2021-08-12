6674d25882a6008b52c84ff2c94c9f301abef223
redpandaathome <leeyumi415@gmail.com>
2021-08-12 18:48:53 +0900

    screenshot organizer

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 fs = require('fs');
 const method = process.argv.slice(2)[0];
 const folder = process.argv.slice(3)[0];
-const ellieOrganizer = require("./lib/organizer-for-ellie.js");
+const ellieOrganizer = require("./lib/ellie-organizer.js");
+const caputreOrganizer = require("./lib/capture-organizer.js")
 main()
 
 // run ex - node app.js ellie test
-// run ex - node app.js screenshot to-be-sorted
+// run ex - node app.js screenshot Screenshot
 
 // FULL_PATH ex - export FULL_PATH="/Users/yumi/Downloads/life/photo/"
 function main(){
    console.log("🥁🥁🥁🥁🥁")
    const fullPath = process.env.FULL_PATH + folder + "/";
+   const deskTopPath = '/Users/yumi/Desktop/';
    console.log(`Processing in ${fullPath}`)
    if (method == 'ellie'){
       ellieOrganizer.organizer(fullPath)
    } else if ( method == 'screenshot'){
-      
+      caputreOrganizer.organizer(deskTopPath, folder)
    }
 }
