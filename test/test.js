bd19934bd561ab7188b366c81a690370632b7f2a
redpandaathome <leeyumi415@gmail.com>
2021-08-18 23:56:25 +0900

    daily organizer - test

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ chai.use(deepEqualInAnyOrder);
 const ellieOrganizer = require("../lib/ellie-organizer.js");
 const helper = require("../lib/sort-helper.js");
 
+// ready to run test? 🧪🏃🏻‍♀️ npm test
 describe("organizer test", () => {
   it("#1 ellie method...", function () {
     mock({
@@ -38,7 +39,7 @@ describe("organizer test", () => {
     ];
 
     const actual = ellieOrganizer.organizer("test/");
-    expect(expected).to.deep.equalInAnyOrder(actual);
+    expect(actual).to.deep.equalInAnyOrder(expected);
     mock.restore();
   });
 
@@ -63,7 +64,50 @@ describe("organizer test", () => {
     ];
 
     const actual = helper.organizer("test/", "organized-test/", "screenshot")
-    expect(expected).to.deep.equalInAnyOrder(actual);
+    expect(actual).to.deep.equalInAnyOrder(expected);
+    mock.restore();
+  });
+
+  it("#3 daily method...", function () {
+    mock({
+      test: {
+        "P1010794.jpg": mock.file({
+          content:"# 2020-10-25",
+          birthtime: new Date('2020-10-25')
+        }),
+        "bbb.png": mock.file({
+          content:"# 2021-05-05",
+          birthtime: new Date('2021-05-05')
+        }),
+        "captured.png": mock.file({
+          content:"# 2021-05-05",
+          birthtime: new Date('2021-05-05')
+        }),
+        "aaa.png": mock.file({
+          content:"# 2021-08-11",
+          birthtime: new Date('2021-08-11')
+        }),
+        "Screen Shot 2021-08-17 at 8.43.42 PM.png": mock.file({
+          content:"# 2021-08-17",
+          birthtime: new Date('2021-08-17')
+        }),
+      },
+    });
+
+    const files = helper.getImageVideoFiles("test/");
+    const beforePaths = helper.getImageVideoFullPaths("test/", files);
+
+    const expected = [
+       "organized-test/2020-10-25/P1010794.jpg",
+       "organized-test/2021-05-05/bbb.png",
+       "organized-test/2021-05-05/captured.png",
+       "organized-test/2021-08-11/aaa.png",
+       "organized-test/2021-08-17/Screen Shot 2021-08-17 at 8.43.42 PM.png",
+    ];
+
+    const actual = helper.organizer("test/", "organized-test/", "daily")
+    console.log("actual:", actual)
+    expect(actual).to.deep.equalInAnyOrder(expected);
     mock.restore();
   });
 });
